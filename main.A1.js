18c4f63c98abb309725eecb8b85031fca1836c88
aanne <aurelien.anne-giffard@wanadoo.fr>
2018-01-26 11:27:41 +0100

    Correction bug camera

--- main.A1.js
+++ main.A1.js
@@ -86,7 +86,6 @@ function start()
 	renderingEnvironment.camera.position.z = 10.0 ;
 	renderingEnvironment.camera.position.y = -10.0 ;
 	renderingEnvironment.camera.rotation.x = 85.0*3.14159/180.0 ;
-	embarque = true;
 
 	//	Skybox
 	Loader.loadSkyBox('assets/maps',['px','nx','py','ny','pz','nz'],'jpg', renderingEnvironment.scene, 'sky',4000);
@@ -144,20 +143,7 @@ function start()
 		currentlyPressedKeys[event.keyCode] = true;
 		if (event.keyCode == 80) // (P) Down 
 		{
-			if(embarque){
-				embarque = false;
-				playerCar.carGeometry.remove(renderingEnvironment.camera);
-				switchCamera(NAV, renderingEnvironment.camera, playerCar.vehicle);
-			}else{
-				embarque = true;
-				playerCar.carGeometry.add(renderingEnvironment.camera) ;
-				renderingEnvironment.camera.position.x = 0.0 ;
-				renderingEnvironment.camera.position.z = 5.0 ;
-				renderingEnvironment.camera.position.y = 0.0 ;
-				renderingEnvironment.camera.rotation.x = 85.0 * Math.PI/180.0 ;
-				renderingEnvironment.camera.rotation.y = 0 * Math.PI/180.0 ;
-				renderingEnvironment.camera.rotation.z = 0.0 * Math.PI/180.0 ;
-			}
+			camera.switch(NAV, renderingEnvironment.camera, playerCar, carGeometry);
 		}
 	}
 	function handleKeyUp(event) {currentlyPressedKeys[event.keyCode] = false;}
@@ -259,8 +245,6 @@ function start()
 		
 		renderVehicule(playerCar);
 		renderAIvehicule();
-		// Camera
-		switchCamera(NAV, renderingEnvironment.camera, playerCar.vehicle);
 		editInfos(NAV, playerCar.vehicle);
 
 //		renderingEnvironment.camera.rotation.z = vehicle.angles.z-Math.PI/2.0 ;
@@ -505,114 +489,4 @@ function saveGhostPosition(NAV, key) {
 		"z": NAV.z,
 		"key": key
 	});
-}
-
-// Caméra
-function switchCamera(NAV, camera, vehicle){
-	if(!embarque){
-		switch(NAV.active) {
-			case "0":
-			case "1":
-				camera.lookAt(NAV);
-				camera.position.x = -220;
-				camera.position.y = -150 ;
-				camera.position.z = 70;
-				camera.rotation.x = 70.0 * Math.PI/180.0;
-				camera.rotation.z = 0.0 * Math.PI/180.0;
-				break;
-			case "2":
-			case "3":
-			case "4":
-				camera.lookAt(NAV);
-				camera.position.x = -250 ;
-				camera.position.y = 260 ;
-				camera.position.z = NAV.z+40;
-				camera.rotation.x = -60.0 * Math.PI/180.0;
-				camera.rotation.z = -165.0 * Math.PI/180.0;
-				break;
-			case "5":
-			case "6":
-			case "7":
-				camera.lookAt(NAV);
-				camera.position.x = -80;
-				camera.position.y = 260 ;
-				camera.position.z = NAV.z+40;
-				camera.rotation.x = -60.0 * Math.PI/180.0;
-				camera.rotation.z = -165.0 * Math.PI/180.0;
-				break;
-			case "8":
-			case "9":
-			case "10":
-				camera.lookAt(NAV);
-				camera.position.x = 30;
-				camera.position.y = 50 ;
-				camera.position.z = 80;
-				camera.rotation.x = 90.0 * Math.PI/180.0;
-				camera.rotation.z = 0.0 * Math.PI/180.0;
-				break;
-			case "11":
-			case "12":
-			case "13":
-			case "14":
-				camera.lookAt(NAV);
-				camera.position.x = 230;
-				camera.position.y = 100 ;
-				camera.position.z = 80;
-				camera.rotation.x = 290.0 * Math.PI/180.0;
-				camera.rotation.z = 180.0 * Math.PI/180.0;
-				break;
-			case "15":
-			case "16":
-			case "17":
-				camera.lookAt(NAV);
-				camera.position.x = 270;
-				camera.position.y = -200 ;
-				camera.position.z = 80;
-				camera.rotation.x = 90.0 * Math.PI/180.0;
-				camera.rotation.z = 0.0 * Math.PI/180.0;
-				break;
-			case "18":
-			case "19":
-			case "20":
-				camera.lookAt(NAV);
-				camera.position.x = 50;
-				camera.position.y = -300 ;
-				camera.position.z = 100;
-				camera.rotation.x = 90.0 * Math.PI/180.0;
-				camera.rotation.z = 0.0 * Math.PI/180.0;
-				break;
-			case "21":
-			case "22":
-			case "23":
-			case "24":
-			case "25":
-				camera.lookAt(NAV);
-				camera.position.x = -40;
-				camera.position.y = 20 ;
-				camera.position.z = 160;
-				camera.rotation.x = 290.0 * Math.PI/180.0;
-				camera.rotation.z = 180.0 * Math.PI/180.0;
-				break;
-			case "26":
-			case "27":
-			case "28":
-			case "29":
-			case "30":
-				camera.lookAt(NAV);
-				camera.position.x = -180;
-				camera.position.y = -330 ;
-				camera.position.z = 100;
-				camera.rotation.x = 70.0 * Math.PI/180.0;
-				camera.rotation.z = 0.0 * Math.PI/180.0;
-				break;
-			default:
-				camera.lookAt(NAV);
-				camera.position.x = NAV.x ;
-				camera.position.y = NAV.y ;
-				camera.position.z = NAV.z+50+vehicle.speed.length()*2 ;
-				camera.rotation.x = 0.0 * Math.PI/180.0;
-				camera.rotation.z = 0.0 * Math.PI/180.0;
-				break;
-    	}
-	}
 }
